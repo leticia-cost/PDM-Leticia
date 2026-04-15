2276f84b167b80b11c7011f5df83a67d2089fc3e
leticia-cost <leticia.alexandre@academico.ifpb.edu.br>
2026-04-15 11:19:53 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.image("let.png")
+st.write("Site Letícia Alexandre")
+st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/leticiaalexandre2informatica/in%C3%ADcio")
