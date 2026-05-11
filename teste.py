fabd68d95784b1201026a7fcb70e84788b42e244
leticia-cost <leticia.alexandre@academico.ifpb.edu.br>
2026-05-11 11:49:34 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
 st.image("let.png")
+st.image("genshim.png.jpg")
 st.write("Site Letícia Alexandre")
 st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/leticiaalexandre2informatica/in%C3%ADcio")
