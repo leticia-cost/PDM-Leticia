058d19465813fc1225d1875a7614c823f3f34dad
leticia-cost <leticia.alexandre@academico.ifpb.edu.br>
2026-05-11 11:53:29 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,6 +3,5 @@ import streamlit as st
 st.image("let.png")
 st.write("Site Letícia Alexandre")
 st.image("genshim.png.jpg")
-st.write("Genshin Impact é um jogo eletrônico RPG de ação gratuito desenvolvido 
-e publicado pela desenvolvedora chinesa de jogos, miHoYo.")
+st.write("Genshin Impact é um jogo eletrônico RPG de ação gratuito desenvolvido e publicado pela desenvolvedora chinesa de jogos, miHoYo.")
 st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/leticiaalexandre2informatica/in%C3%ADcio")
